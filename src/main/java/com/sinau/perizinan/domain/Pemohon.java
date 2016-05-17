2a2b2f1717fee8b5ba4c1a8b548060cc26e6d12a
herusantoso23 <herusantoso008@gmail.com>
2016-05-17 12:51:07 +0700

    update PerizinanPathMappingConstants, PemohonController, Pemohon

--- src/main/java/com/sinau/perizinan/domain/Pemohon.java
+++ src/main/java/com/sinau/perizinan/domain/Pemohon.java
@@ -1,20 +1,184 @@
 package com.sinau.perizinan.domain;
 
 public class Pemohon {
-	private String idPemohon;
-	private String badanUsaha;
-	private String namaUsaha;
+	//detail yang mendaftarkan
+	private String nama;
+	private String nomorKtp;
+	private String alamatWakil;
 	private String jenisPemohon;
+	//detail pemohon
+	private String idPemohon;
 	private String namaPemohon;
 	private String jenisKelamin;
+	private String tempatLahir;
+	private String tanggalLahir;
+	private String npwp;
+	private String noKtpPemohon;
+	private String jenisIdentitasLain;
+	private String noIdentitas;
+	private String noHp;
+	private String email;
+	private String pekerjaan;
 	private String alamat;
-	
+	private String propinsi;
+	private String kota;
+	private String kecamatan;
+	private String kelurahanDesa;
+	private String password;
+	//detail perusahaan
+	private String badanUsaha;
+	private String namaUsaha;
+
+	//setter getter detail yang mendaftarkan
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getNomorKtp() {
+		return nomorKtp;
+	}
+	public void setNomorKtp(String nomorKtp) {
+		this.nomorKtp = nomorKtp;
+	}
+	public String getAlamatWakil() {
+		return alamatWakil;
+	}
+	public void setAlamatWakil(String alamatWakil) {
+		this.alamatWakil = alamatWakil;
+	}
+	public String getJenisPemohon() {
+		return jenisPemohon;
+	}
+	public void setJenisPemohon(String jenisPemohon) {
+		this.jenisPemohon = jenisPemohon;
+	}
+
+	//setter getter detail pemohon
 	public String getIdPemohon() {
 		return idPemohon;
 	}
 	public void setIdPemohon(String idPemohon) {
 		this.idPemohon = idPemohon;
+	}public String getNamaPemohon() {
+		return namaPemohon;
 	}
+	public void setNamaPemohon(String namaPemohon) {
+		this.namaPemohon = namaPemohon;
+	}
+	public String getJenisKelamin() {
+		return jenisKelamin;
+	}
+	public void setJenisKelamin(String jenisKelamin) {
+		this.jenisKelamin = jenisKelamin;
+	}
+	public String getTempatLahir() {
+		return tempatLahir;
+	}
+	public void setTempatLahir(String tempatLahir) {
+		this.tempatLahir = tempatLahir;
+	}
+	public String getTanggalLahir() {
+		return tanggalLahir;
+	}
+	public void setTanggalLahir(String tanggalLahir) {
+		this.tanggalLahir = tanggalLahir;
+	}
+	public String getNpwp() {
+		return npwp;
+	}
+	public void setNpwp(String npwp) {
+		this.npwp = npwp;
+	}
+	public String getNoKtpPemohon() {
+		return noKtpPemohon;
+	}
+	public void setNoKtpPemohon(String noKtpPemohon) {
+		this.noKtpPemohon = noKtpPemohon;
+	}
+	public String getJenisIdentitasLain() {
+		return jenisIdentitasLain;
+	}
+	public void setJenisIdentitasLain(String jenisIdentitasLain) {
+		this.jenisIdentitasLain = jenisIdentitasLain;
+	}
+	public String getNoIdentitas() {
+		return noIdentitas;
+	}
+	public void setNoIdentitas(String noIdentitas) {
+		this.noIdentitas = noIdentitas;
+	}
+	public String getNoHp() {
+		return noHp;
+	}
+	public void setNoHp(String noHp) {
+		this.noHp = noHp;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPekerjaan() {
+		return pekerjaan;
+	}
+	public void setPekerjaan(String pekerjaan) {
+		this.pekerjaan = pekerjaan;
+	}
+
+	public String getAlamat() {
+		return alamat;
+	}
+	public void setAlamat(String alamat) {
+		this.alamat = alamat;
+	}
+	public String getPropinsi() {
+		return propinsi;
+	}
+	public void setPropinsi(String propinsi) {
+		this.propinsi = propinsi;
+	}
+	public String getKota() {
+		return kota;
+	}
+	public void setKota(String kota) {
+		this.kota = kota;
+	}
+	public String getKecamatan() {
+		return kecamatan;
+	}
+	public void setKecamatan(String kecamatan) {
+		this.kecamatan = kecamatan;
+	}
+	public String getKelurahanDesa() {
+		return kelurahanDesa;
+	}
+	public void setKelurahanDesa(String kelurahanDesa) {
+		this.kelurahanDesa = kelurahanDesa;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	public String getBadanUsaha() {
 		return badanUsaha;
 	}
@@ -27,28 +191,7 @@ public class Pemohon {
 	public void setNamaUsaha(String namaUsaha) {
 		this.namaUsaha = namaUsaha;
 	}
-	public String getJenisPemohon() {
-		return jenisPemohon;
-	}
-	public void setJenisPemohon(String jenisPemohon) {
-		this.jenisPemohon = jenisPemohon;
-	}
-	public String getNamaPemohon() {
-		return namaPemohon;
-	}
-	public void setNamaPemohon(String namaPemohon) {
-		this.namaPemohon = namaPemohon;
-	}
-	public String getJenisKelamin() {
-		return jenisKelamin;
-	}
-	public void setJenisKelamin(String jenisKelamin) {
-		this.jenisKelamin = jenisKelamin;
-	}
-	public String getAlamat() {
-		return alamat;
-	}
-	public void setAlamat(String alamat) {
-		this.alamat = alamat;
-	}	
+
+
+
 }
